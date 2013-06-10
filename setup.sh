ada4598488f52d2260509cce5fb97a36682e3acd
huiqing <hl@kent.ac.uk>
2013-06-10 12:16:17 +0100

    a shell script starting nodes for testing; should make it more elegant.

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+c:/erl5.9.1/bin/erl -name node1@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node2@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node3@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node4@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node5@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node6@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node7@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node8@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node9@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node10@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node11@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node12@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node13@127.0.0.1 -detached -setcookie "secret" -config s_group.config
+c:/erl5.9.1/bin/erl -name node14@127.0.0.1 -detached -setcookie "secret" -config s_group.config
